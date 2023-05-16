ca098fa2f23e3d1131c4133769456a70b066cbee
freshcodeAlice <freshjs.ofte@gmail.com>
2023-05-16 09:27:39 +0300

    repl

--- index.js
+++ index.js
@@ -1,38 +1,13 @@
-const cart = {
-    userId: 3,
-    date: '2023-05-15',
-    products:[{productId:1,quantity:3}]
-}
+/*
+REPL
 
-
-
-fetch('https://fakestoreapi.com:443/carts/7', {
-    method: 'PUT',
-    headers: {
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify(cart)
-})
-.then(res => res.json())
-.then(data => console.log(data));
-
-
-/* GET-request
-GET https://fakestoreapi.com:443/products/1  HTTPS
-Content-type: application/json
-
-*/
-
-/* PUT-request
-
-PUT 'https://fakestoreapi.com:443/carts/7' HTTPS
-Content-type: application/json
-
-{
-    "firstName": "John",
-    "lastName": "Doe"
-}
-
+Read
+Eval
+Print
+Loop
 
 
 */
+
+
+console.log(2+2);
