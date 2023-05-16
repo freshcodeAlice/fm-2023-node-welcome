c1579709a8e7401780938ef9937b33e13e57b6bc
freshcodeAlice <freshjs.ofte@gmail.com>
2023-05-16 11:18:45 +0300

    filesystem read and write files

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const fs = require('fs/promises');
 
-fs.readFile('./texts/citadelle.txt', {encoding: 'utf-8'})
+fs.readFile('./texts/citadelle.txt', 'utf-8')
 .then(result => {
    const quotesArray = result.split('\n').filter(el => el);
    const randomIndex = Math.ceil(Math.random() * (quotesArray.length - 1));
-   console.log(quotesArray[randomIndex]);
+//    console.log(quotesArray[randomIndex]);
+    // fs.writeFile('./quote.txt', quotesArray[randomIndex]);
+    fs.appendFile('./anotherQuote.txt', quotesArray[randomIndex] + '\n', 'utf-8');
 })
+
+
+// fs.writeFile('./quote.txt', 'Some another quote');
