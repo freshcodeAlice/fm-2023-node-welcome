c416ff2305d4e6e3d7a8594880b51134fd407b9e
freshcodeAlice <freshjs.ofte@gmail.com>
2023-05-16 11:47:02 +0300

    error logger task resolve

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const fs = require('fs/promises');
 
-fs.readFile('./texts/citadelle.txt', 'utf-8')
-.then(result => {
-   const quotesArray = result.split('\n').filter(el => el);
-   const randomIndex = Math.ceil(Math.random() * (quotesArray.length - 1));
-//    console.log(quotesArray[randomIndex]);
-    // fs.writeFile('./quote.txt', quotesArray[randomIndex]);
-    fs.appendFile('./anotherQuote.txt', quotesArray[randomIndex] + '\n', 'utf-8');
-})
+/*
+Написати функцію, яка раз в секунду дописує до файлу-логгеру рядок з текстом вигляду: "Some error happens at 2023-05-16T08:19:55.285Z"
 
 
-// fs.writeFile('./quote.txt', 'Some another quote');
+Повторити це протягом 10 секунд
+
+*/
+
+function log() {
+    fs.appendFile('./error-logger.txt', `Some error happens at ${new Date()} \n`, 'utf-8');
+}
+
+function createTimerLogger () {
+
+    let i = 0;
+    setTimeout(function repeat() {
+        i++;
+        log();
+        if(i < 10) {
+            let timeout = setTimeout(repeat, 1000);
+        } 
+    }, 1000)
+    
+}
+
+createTimerLogger();
+
+
