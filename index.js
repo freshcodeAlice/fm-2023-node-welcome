78e4d89ea7d67ee467d71fc4ad27faaea9c262b5
freshcodeAlice <freshjs.ofte@gmail.com>
2023-05-17 11:18:47 +0300

    post request (chunk reading)

--- index.js
+++ index.js
@@ -6,33 +6,64 @@ const server = http.createServer(requestListener);
 let counter = 0;
 
 function requestListener(request, response) {
-   console.log(request.url)
-  if(request.url === '/home') {
-    fs.readFile('./views/index.html', 'utf-8')
-    .then(data => {
-        response.statusCode = 200;
-        response.end(data);
-    })
-  } else if (request.url === '/style.css') {
-    fs.readFile('./views/style.css', 'utf-8')
-    .then(data => {
-        response.end(data);
-    })
-  } else {
-    response.statusCode = 404;
-    response.end('File not found')
-  }
+    if(request.method === 'GET') {
+        if(request.url === '/home') {
+            fs.readFile('./views/index.html', 'utf-8')
+            .then(data => {
+                response.statusCode = 200;
+                response.end(data);
+            })
+          } else if (request.url === '/style.css') {
+            fs.readFile('./views/style.css', 'utf-8')
+            .then(data => {
+                response.end(data);
+            })
+          } else {
+            response.statusCode = 404;
+            fs.readFile('./views/404.html', 'utf-8')
+            .then(data => {
+                response.end(data);
+            })
+          }
+    } else if (request.method === 'POST') {
+        if(request.url === '/users'){
+            /* Отримання даних відбувається в асинхронному форматі за допомогою ReadableStream.
+                +Коли дані приходять, стається подія 'data'
+                Коли дані всі прийшли, стається подія 'end'
+            */
 
-  /// Опрацювати запит на /style.css і віддати його у відповідь
+                let stringFromChunk = '';
+                request.on('data', (chunk) => {
+                    stringFromChunk += chunk
+                });
+
+                request.on('end', async () => {
+                    const userData = JSON.parse(stringFromChunk);
+                    
+                    try {
+                        await writeUserToFile(userData);
+                        response.statusCode = 201;
+                        response.end('User successfully created')
+                    } catch (error) {
+                        response.statusCode = 400;
+                        response.end('Oops');
+                    }
+
+                })
+        }
+    }
 }
 
 server.listen(3000);
 
 
 /*
-Задача: при запиті на localhost:3000/home віддавати статичний файлик index.html
 
-1. Дізнатись, що запит був дійсно за тим шляхом, на який ми очікували
-2. Віддати у відповідь статичний файл
-
+Задача: зареєструвати нового юзера.
+*прийняти дані (логін, пароль, ім'я, прізвище) і записати їх у .txt
 */
+
+
+async function writeUserToFile(userData) {
+    return fs.appendFile('./data/userData.txt', JSON.stringify(userData), 'utf-8');
+}
