3cab89ac5fccddbf81f570d40331491b45ec1d89
freshcodeAlice <freshjs.ofte@gmail.com>
2023-05-17 11:46:12 +0300

    login task resolve

--- index.js
+++ index.js
@@ -30,16 +30,16 @@ function requestListener(request, response) {
             /* Отримання даних відбувається в асинхронному форматі за допомогою ReadableStream.
                 +Коли дані приходять, стається подія 'data'
                 Коли дані всі прийшли, стається подія 'end'
-            */
-
+            */  
+            
                 let stringFromChunk = '';
+
                 request.on('data', (chunk) => {
                     stringFromChunk += chunk
                 });
-
+            
                 request.on('end', async () => {
                     const userData = JSON.parse(stringFromChunk);
-                    
                     try {
                         await writeUserToFile(userData);
                         response.statusCode = 201;
@@ -48,8 +48,36 @@ function requestListener(request, response) {
                         response.statusCode = 400;
                         response.end('Oops');
                     }
-
                 })
+                   
+
+        } else if (request.url === '/login') {
+            let stringFromChunk = '';
+            request.on('data', (chunk) => {
+                stringFromChunk += chunk
+            });
+
+            request.on('end', async () => {
+                const userData = JSON.parse(stringFromChunk);
+                
+                try {
+                   const readedText = await readUserData();
+                   const savedData = JSON.parse(readedText);
+
+                   if(userData.email === savedData.email && userData.password === savedData.password) {
+                    response.statusCode = 200;
+                    response.end('Login successfull');
+                   } else {
+                    response.statusCode = 401;
+                    response.end('User data is invalid');
+                   }
+
+                } catch (error) {
+                    response.statusCode = 400;
+                    response.end('Oops');
+                }
+
+            })
         }
     }
 }
@@ -66,4 +94,15 @@ server.listen(3000);
 
 async function writeUserToFile(userData) {
     return fs.appendFile('./data/userData.txt', JSON.stringify(userData), 'utf-8');
+}
+
+/*
+Створити файл з мейлом і паролем юзера
+Прийняти запит на адресу /login з методом POST. Запит має містити логін та пароль
+Зчитати вміст файлу, розпарсити як js-об'єкт.
+Перевірити, чи співпадають дані, переслані з клієнта з тими даними які ви зчитали   
+*/
+
+async function readUserData() {
+    return fs.readFile('./data/loginData.txt', 'utf-8');
 }
