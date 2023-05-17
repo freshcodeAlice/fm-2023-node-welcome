40842d54c52646b2aca64023a1c3f0ea3520154a
freshcodeAlice <freshjs.ofte@gmail.com>
2023-05-17 09:39:41 +0300

    http createServer

--- index.js
+++ index.js
@@ -1,41 +1,15 @@
-const fs = require('fs/promises');
+const http = require('http');
 
-/*
-Написати функцію, яка раз в секунду дописує до файлу-логгеру рядок з текстом вигляду: "Some error happens at 2023-05-16T08:19:55.285Z"
+const server = http.createServer(requestListener);
 
-
-Повторити це протягом 10 секунд
-
-*/
-
-function log() {
-    fs.appendFile('./error-logger.txt', `Some error happens at ${new Date()} \n`, 'utf-8');
+function requestListener(request, response) {
+    response.end('Hello from node');
 }
 
-function createTimerLogger () {
-
-    let i = 0;
-    setTimeout(function repeat() {
-        i++;
-        log();
-        if(i < 10) {
-            let timeout = setTimeout(repeat, 1000);
-        } 
-    }, 1000)
-    
-}
-
-//createTimerLogger();
-
+server.listen(3000);
 
 
 /*
-Створити файл з будь-яким текстовим вмістом. Прочитати цей файл, і створити новий, який складатиметься з інформації старого файлу + якогось нового тексту
-
+Напишіть сервер, який на запити відповідає числом запитів, які вже були зроблені на цей сервер
 
 */
-
-fs.readFile('./oldText.txt', 'utf-8')
-.then(result => {
-    fs.writeFile('./newFile.txt', result + '\n New file content added in the end', 'utf-8');
-})
