0dde126059666e60d1d101782a8dc6df40c86679
freshcodeAlice <freshjs.ofte@gmail.com>
2023-05-18 09:33:40 +0300

    express welcome!

--- index.js
+++ index.js
@@ -1,108 +1,24 @@
-const http = require('http');
-const fs = require('fs/promises');
+const express = require('express');
 
-const server = http.createServer(requestListener);
+const app = express();
 
-let counter = 0;
-
-function requestListener(request, response) {
-    if(request.method === 'GET') {
-        if(request.url === '/home') {
-            fs.readFile('./views/index.html', 'utf-8')
-            .then(data => {
-                response.statusCode = 200;
-                response.end(data);
-            })
-          } else if (request.url === '/style.css') {
-            fs.readFile('./views/style.css', 'utf-8')
-            .then(data => {
-                response.end(data);
-            })
-          } else {
-            response.statusCode = 404;
-            fs.readFile('./views/404.html', 'utf-8')
-            .then(data => {
-                response.end(data);
-            })
-          }
-    } else if (request.method === 'POST') {
-        if(request.url === '/users'){
-            /* Отримання даних відбувається в асинхронному форматі за допомогою ReadableStream.
-                +Коли дані приходять, стається подія 'data'
-                Коли дані всі прийшли, стається подія 'end'
-            */  
-            
-                let stringFromChunk = '';
-
-                request.on('data', (chunk) => {
-                    stringFromChunk += chunk
-                });
-            
-                request.on('end', async () => {
-                    const userData = JSON.parse(stringFromChunk);
-                    try {
-                        await writeUserToFile(userData);
-                        response.statusCode = 201;
-                        response.end('User successfully created')
-                    } catch (error) {
-                        response.statusCode = 400;
-                        response.end('Oops');
-                    }
-                })
-                   
-
-        } else if (request.url === '/login') {
-            let stringFromChunk = '';
-            request.on('data', (chunk) => {
-                stringFromChunk += chunk
-            });
-
-            request.on('end', async () => {
-                const userData = JSON.parse(stringFromChunk);
-                
-                try {
-                   const readedText = await readUserData();
-                   const savedData = JSON.parse(readedText);
-
-                   if(userData.email === savedData.email && userData.password === savedData.password) {
-                    response.statusCode = 200;
-                    response.end('Login successfull');
-                   } else {
-                    response.statusCode = 401;
-                    response.end('User data is invalid');
-                   }
-
-                } catch (error) {
-                    response.statusCode = 400;
-                    response.end('Oops');
-                }
-
-            })
-        }
-    }
-}
-
-server.listen(3000);
-
+app.get('/index', handler);
+app.post('/users', userHandler);
 
 /*
-
-Задача: зареєструвати нового юзера.
-*прийняти дані (логін, пароль, ім'я, прізвище) і записати їх у .txt
+Endpoint (ручка) - метод + шлях запиту
 */
 
+function handler(req, res) {
+    console.log(req.url);
+    res.end('hello, world');
 
-async function writeUserToFile(userData) {
-    return fs.appendFile('./data/userData.txt', JSON.stringify(userData), 'utf-8');
 }
 
-/*
-Створити файл з мейлом і паролем юзера
-Прийняти запит на адресу /login з методом POST. Запит має містити логін та пароль
-Зчитати вміст файлу, розпарсити як js-об'єкт.
-Перевірити, чи співпадають дані, переслані з клієнта з тими даними які ви зчитали   
-*/
 
-async function readUserData() {
-    return fs.readFile('./data/loginData.txt', 'utf-8');
+function userHandler(req, res) {
+    res.status(400).end('user cannot be created')
 }
+
+
+app.listen(3000);
